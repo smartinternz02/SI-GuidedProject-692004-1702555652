6107eae45bb07dd1badf4f6fb9995aa98bf586b3
lithilsagar <150673776+lithilsagar@users.noreply.github.com>
2024-04-19 21:39:16 +0530

    Add files via upload

---
+++ major project/flask/app.p.py
@@ -0,0 +1,49 @@
+# app.py
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+from lane import canny, region_of_interest, display_lines, average_slope_intercept
+
+app = Flask(__name__)
+
+def detect_lane(frame):
+    canny_image = canny(frame)
+    cropped_image = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_image = display_lines(frame, averaged_lines)
+    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    return combo_image
+
+def generate_frames():
+    cap = cv2.VideoCapture("test2.mp4")
+
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        processed_frame = detect_lane(frame)
+
+        ret, buffer = cv2.imencode('.jpg', processed_frame)
+        frame_bytes = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
